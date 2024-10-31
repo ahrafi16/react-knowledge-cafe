276ae1c5dd03d1bc5fd702e0115b197d2945475e
ahrafi16 <anjumhossianrafi16@gmail.com>
2024-10-31 17:00:56 +0600

    added blogs

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,14 +1,36 @@
 import PrpoTypes from 'prop-types';
-const Blog = ({blog}) => {
+import { FaBookmark } from 'react-icons/fa';
+import { FaBookBookmark } from 'react-icons/fa6';
+const Blog = ({ blog }) => {
+    const {title, cover, author, author_img, reading_time, posted_date, hashtags} = blog; 
     return (
-        <div>
-            
+        <div className='mb-20'>
+            <img className='w-full mb-8 rounded-2xl' src={cover} alt={`Cover picture of the title ${title}`} />
+            <div className='flex justify-between mb-4'>
+                <div className='flex'>  
+                    <img className='w-14 rounded-full object-cover' src={author_img} alt="" />
+                    <div className='ml-6'>
+                        <h3 className='text-2xl'>{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div className='flex items-center'>
+                    <span>{reading_time} min read</span>
+                    <button className='ml-2 text-red-600 text-xl'><FaBookBookmark></FaBookBookmark></button>
+                </div>
+            </div>
+            <h2 className="text-4xl">{title}</h2>
+            <p>
+                {
+                    hashtags.map((hash, idx) => <span key={idx}><a href="">#{hash}</a></span>)
+                }
+            </p>
         </div>
     );
 };
 
-Blog.propTypes {
-    blog : PrpoTypes.object.isRequired
+Blog.propTypes = {
+    blog: PrpoTypes.object.isRequired
 }
 
 export default Blog;
--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -1,16 +1,21 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Blog from "../Blog/Blog";
 
 const Blogs = () => {
-    const {blogs, setBlogs} = useState([])
-    useEffect(()=>{
+    const [blogs, setBlogs] = useState([])
+    useEffect(() => {
         fetch('blogs.json')
-        .then(res => res.json())
-        .then(data => setBlogs(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setBlogs(data))
+    }, [])
+
     return (
         <div className="md:w-2/3">
-            <h1 className="text4xl">Blogs</h1>
+            <h1>Blogs : {blogs.length}</h1>
+            {
+                blogs.map(blog => <Blog key={blog.id} blog={blog}></Blog>)
+            }
         </div>
     );
 };
